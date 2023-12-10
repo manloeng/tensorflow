c27afbb06f9349f80392528fbbcb1aeb1187d497
Andrew <manloengchung@yahoo.co.uk>
2023-12-10 14:38:42 +0700

    rename file

--- helper_function/data_prep/performace.py
+++ helper_function/data_prep/performace.py
@@ -2,8 +2,16 @@ import tensorflow as tf
 
 BATCH_SIZE = 1024
 
+
+def combine_dataset(features: tf.data.Dataset, labels: tf.data.Dataset):
+    """
+    Combine labels and features by zipping together -> (features, labels)
+    """
+    return tf.data.Dataset.zip((features, labels))
+
+
 # Batch and prefetch
-def increase_dataset_performance(train_dataset, test_dataset):
+def increase_dataset_performance(train_dataset: tf.data.Dataset.zip, test_dataset: tf.data.Dataset.zip):
     """
     For more detail: https://www.tensorflow.org/guide/data_performance
     """
